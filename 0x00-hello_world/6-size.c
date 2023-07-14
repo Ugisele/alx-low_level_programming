9d4bc2923c66ed3fe890e58a808478864a837688
Umutoni Gisele <92855186+Ugisele@users.noreply.github.com>
2023-07-13 18:28:08 -0700

    Delete 5-printf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - print the size
- * return: 0 (success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
