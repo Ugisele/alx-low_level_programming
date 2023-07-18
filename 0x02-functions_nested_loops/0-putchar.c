326ec79eaa27a570252737c4e9b68954ab5ad413
Ugisele <umutonigisele12@gmail.com>
2023-07-18 09:16:22 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char g = "_putchar";
+
+	while (g)
+	{
+		_putchar(g);
+		g++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
+
