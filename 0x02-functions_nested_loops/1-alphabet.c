c3ee4bcc3d883b1ca91b431c5ed1abcf420959aa
Ugisele <umutonigisele12@gmail.com>
2023-07-18 12:58:02 -0700

    print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - Make the alphabet
+  *
+  * Return: void
+  */
+void print_alphabet(void)
+{
+	char (g);
+	for (g = 'a'; g <= 'z'; g++)
+	{
+		_putchar(g);
+	}
+	_putchar('\n');
+}
