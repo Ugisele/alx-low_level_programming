f995d11c1167a1854a11460ba7d7b20d5cedd973
Ugisele <umutonigisele12@gmail.com>
2023-08-28 12:27:13 -0700

    lis telement

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,10 +1,8 @@
-lists.h
-
 #ifndef LISTS_H
 #define LISTS_H
 
 #include <stdlib.h>
-
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -34,4 +32,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif
+#endif /*lists-h*/
